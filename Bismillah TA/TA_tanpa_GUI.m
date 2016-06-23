9f48f8ede544036ca5efb147c5dcd0b631905c38
mustofa27 <buffonmustofa@gmail.com>
2016-06-24 05:21:17 +0700

    Bismillah Final

--- Bismillah TA/TA_tanpa_GUI.m
+++ Bismillah TA/TA_tanpa_GUI.m
@@ -8,6 +8,9 @@ if (user_canceled == 0)
     [row,col]=find(disk);
     row_t = min(row);
     col_t = min(col);
+    disk1 = disk;
+    cup1 = cup;
+    blood1 = blood;
     %%%%%%%%%%%%%
     disk = imcrop(disk,[col_t row_t max(col)-col_t max(row)-row_t]);
     %%%%%%%%%%%%%
@@ -33,11 +36,11 @@ if (user_canceled == 0)
     
     subplot(3,3,1),imshow(I(:,:,1)),title('Red Channel');
     subplot(3,3,2),imshow(Preprocessing(I(:,:,1),'disk')),title('Preprocessed disk');
-    subplot(3,3,3),imshow(disk),title('Segmented disk');
+    subplot(3,3,3),imshow(disk1),title('Segmented disk');
     subplot(3,3,4),imshow(I(:,:,2)),title('Green Channel');
     subplot(3,3,5),imshow(green),title('Preprocessed cup');
-    subplot(3,3,6),imshow(cup),title('Segmented cup');
-    subplot(3,3,7),imshow(blood),title('Blood Vessel');
+    subplot(3,3,6),imshow(cup1),title('Segmented cup');
+    subplot(3,3,7),imshow(blood1),title('Blood Vessel');
     subplot(3,3,9),imshow(imNrr),title('Neuro Retinal Rim');
     subplot(3,3,8),text(0.2,0.5,hasil);
 end
